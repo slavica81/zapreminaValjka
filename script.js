f9d0f0c8be9c7fb75b188444e25af313eb90b71e
slavica81 <119537487+slavica81@users.noreply.github.com>
2023-10-26 19:54:19 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+       var r = prompt("Unesite poluprecnik:");
+        var h = prompt("Unesite visinu:");
+        var pi = 3.141592;
+        
+
+        var sum = pi * parseInt(r) * parseInt(r) * parseInt(h);
+        alert("Zapremina valjka iznosi: " + sum);
+    
